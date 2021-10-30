3e6a8b3b13427e3bf27dbb5f97b7eabec73fda5e
yashym3171 <93377707+yashym3171@users.noreply.github.com>
2021-10-30 22:29:06 +0530

    update bts with c

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -1,118 +1,87 @@
-//Program to create BST and perform Preorder,Inorder, and Postorder Traversing.
+// Recursive C program for level
+// order traversal of Binary Tree
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<stdio.h>
-struct node
-{
-    int info;
-    struct node *left, *right;
+/* A binary tree node has data,
+pointer to left child
+and a pointer to right child */
+struct node {
+	int data;
+	struct node *left, *right;
 };
-struct node *root=NULL;
-void Create();
-void Preorder(struct node *);
-void Inorder(struct node *);
-void Postorder(struct node *);
+
+/* Function prototypes */
+void printCurrentLevel(struct node* root, int level);
+int height(struct node* node);
+struct node* newNode(int data);
+
+/* Function to print level order traversal a tree*/
+void printLevelOrder(struct node* root)
+{
+	int h = height(root);
+	int i;
+	for (i = 1; i <= h; i++)
+		printCurrentLevel(root, i);
+}
+
+/* Print nodes at a current level */
+void printCurrentLevel(struct node* root, int level)
+{
+	if (root == NULL)
+		return;
+	if (level == 1)
+		printf("%d ", root->data);
+	else if (level > 1) {
+		printCurrentLevel(root->left, level - 1);
+		printCurrentLevel(root->right, level - 1);
+	}
+}
+
+/* Compute the "height" of a tree -- the number of
+	nodes along the longest path from the root node
+	down to the farthest leaf node.*/
+int height(struct node* node)
+{
+	if (node == NULL)
+		return 0;
+	else {
+		/* compute the height of each subtree */
+		int lheight = height(node->left);
+		int rheight = height(node->right);
+
+		/* use the larger one */
+		if (lheight > rheight)
+			return (lheight + 1);
+		else
+			return (rheight + 1);
+	}
+}
+
+/* Helper function that allocates a new node with the
+given data and NULL left and right pointers. */
+struct node* newNode(int data)
+{
+	struct node* node
+		= (struct node*)malloc(sizeof(struct node));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+
+	return (node);
+}
+
+/* Driver program to test above functions*/
 int main()
 {
-    int ch;
-    do
-    {
-        printf("\n\nMENU");
-        printf("\n1 Create\n2 Preorder \n3 Inorder\n4 Postorder");
-        printf("\n5 Exit\nEnter your choice ");
-        scanf("%d",&ch);
-        switch(ch)
-        {
-        case 1:
-            Create();
-            break;
-        case 2:
-            printf("\nPreorder is \n");
-            Preorder(root);
+	struct node* root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
 
+	printf("Level Order traversal of binary tree is \n");
+	printLevelOrder(root);
 
-
-            break;
-        case 3:
-            printf("\nInorder is \n");
-            Inorder(root);
-            break;
-        case 4:
-            printf("\nPostorder is \n");
-            Postorder(root);
-            break;
-        case 5:
-            break;
-        default:
-            printf("\nInvalid choice ");
-        }
-    }
-    while(ch!=5);
-    return 0;
-} // end of main
-void Create()
-{
-    int item;
-    struct node *n, *t, *p;
-    char ch;
-    do
-    {
-        printf("\n\nEnter the number ");
-        scanf("%d",&item);
-//find parent node of item
-        p=NULL;
-        t=root;
-        while(t!=NULL)
-        {
-            p=t;
-            if(item<t->info)
-                t=t->left;
-            else
-                t=t->right;
-
-        }
-//create a new node
-        n = (struct node *) malloc(sizeof(struct node));
-        n->info=item;
-        n->left=NULL;
-        n->right=NULL;
-//insert new node
-        if(root==NULL) //if root is null then new node is the root
-            root=n;
-        else if(item<p->info) //if item is smaller then set in left of parent
-            p->left=n;
-        else
-            p->right=n; //if item is greater then set in right of parent
-        printf("%d is inserted in BST\n",item);
-        printf("\nAdd Another(y/n)");
-        ch=getche();
-    }
-    while(ch=='y' || ch=='Y');
-} // end of Create
-void Preorder(struct node *t)
-{
-    if(t!=NULL)
-    {
-        printf("\t %d ",t->info);
-        Preorder(t->left);
-        Preorder(t->right);
-    }
-}
-
-void Postorder(struct node *t)
-{
-    if(t!=NULL)
-    {
-        Postorder(t->left);
-        Postorder(t->right);
-        printf("\t %d ",t->info);
-    }
-}
-void Inorder(struct node *t)
-{
-    if(t!=NULL)
-    {
-        Inorder(t->left);
-        printf("\t %d ",t->info);
-        Inorder(t->right);
-    }
+	return 0;
 }
